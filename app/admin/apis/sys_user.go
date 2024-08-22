cb83509172ddebd8461d5304bdfdbae09c53fa9f
mingri <3116430062@qq.com>
2024-08-22 17:29:30 +0800

    打卡项目第二次提交

--- app/admin/apis/sys_user.go
+++ app/admin/apis/sys_user.go
@@ -1,26 +1,40 @@
 package apis
 
 import (
+	"context"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-admin-team/go-admin-core/sdk/api"
 	"github.com/go-admin-team/go-admin-core/sdk/pkg/jwtauth/user"
 	_ "github.com/go-admin-team/go-admin-core/sdk/pkg/response"
 	"github.com/google/uuid"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 	"go-admin/app/admin/common"
 	"go-admin/app/admin/models"
 	"go-admin/app/admin/service"
 	"go-admin/app/admin/service/dto"
-	"go-admin/app/admin/utils"
 	"go-admin/common/actions"
 	"golang.org/x/crypto/bcrypt"
+	"log"
 	"net/http"
-	"time"
+	"path/filepath"
+	"strconv"
 )
 
 type SysUser struct {
 	api.Api
 }
+
+// @todo 最终在setting.dev.yml中配置
+var (
+	bucketName      = "clock-bucket"
+	endpoint        = "116.205.189.126:9000"
+	accessKeyID     = "KWZbfWQyLhVTVV4BrGZj"
+	secretAccessKey = "4vNBVT2M1nwqQkGBvmbeDPWlfV1nbbauKqjxFbwM"
+	useSSL          = false
+)
 
 // GetPage
 // @Summary 列表用户信息数据
@@ -207,9 +221,55 @@ func (e SysUser) Delete(c *gin.Context) {
 // @Success 200 {object} response.Response "{"code": 200, "data": [...]}"
 // @Router /api/v1/user/avatar [post]
 // @Security Bearer
+// @头像存入本地/static/uploadfile/文件夹
+
+//func (e SysUser) InsetAvatar(c *gin.Context) {
+//	s := service.SysUser{}
+//	req := dto.UpdateSysUserAvatarReq{}
+//	err := e.MakeContext(c).
+//		MakeOrm().
+//		MakeService(&s.Service).
+//		Errors
+//	if err != nil {
+//		e.Logger.Error(err)
+//		e.Error(500, err, err.Error())
+//		return
+//	}
+//	// 数据权限检查
+//	p := actions.GetPermissionFromContext(c)
+//	form, _ := c.MultipartForm()
+//	files := form.File["upload[]"]
+//	guid := uuid.New().String()
+//	filPath := "static/uploadfile/" + guid + ".jpg"
+//	for _, file := range files {
+//		e.Logger.Debugf("upload avatar file: %s", file.Filename)
+//		// 上传文件至指定目录
+//		err = c.SaveUploadedFile(file, filPath)
+//		if err != nil {
+//			e.Logger.Errorf("save file error, %s", err.Error())
+//			e.Error(500, err, "")
+//			return
+//		}
+//	}
+//	req.UserId = p.UserId
+//	req.Avatar = "/" + filPath
+//
+//	err = s.UpdateAvatar(&req, p)
+//	if err != nil {
+//		e.Logger.Error(err)
+//		return
+//	}
+//	e.OK(filPath, "修改成功")
+//}
+
+/**
+ * 头像存入minio
+ */
 func (e SysUser) InsetAvatar(c *gin.Context) {
 	s := service.SysUser{}
 	req := dto.UpdateSysUserAvatarReq{}
+	urlPrefix := fmt.Sprintf("%s://%s/", "http", endpoint)
+
 	err := e.MakeContext(c).
 		MakeOrm().
 		MakeService(&s.Service).
@@ -219,31 +279,58 @@ func (e SysUser) InsetAvatar(c *gin.Context) {
 		e.Error(500, err, err.Error())
 		return
 	}
-	// 数据权限检查
+
+	minioClient, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: useSSL,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Data permission check
 	p := actions.GetPermissionFromContext(c)
 	form, _ := c.MultipartForm()
 	files := form.File["upload[]"]
-	guid := uuid.New().String()
-	filPath := "static/uploadfile/" + guid + ".jpg"
+
 	for _, file := range files {
 		e.Logger.Debugf("upload avatar file: %s", file.Filename)
-		// 上传文件至指定目录
-		err = c.SaveUploadedFile(file, filPath)
+
+		// Generate a unique file name
+		guid := uuid.New().String()
+		fileExtension := filepath.Ext(file.Filename)
+		fileName := guid + fileExtension
+
+		// Open the uploaded file
+		src, err := file.Open()
 		if err != nil {
-			e.Logger.Errorf("save file error, %s", err.Error())
+			e.Logger.Errorf("file open error, %s", err.Error())
 			e.Error(500, err, "")
 			return
 		}
-	}
-	req.UserId = p.UserId
-	req.Avatar = "/" + filPath
+		defer src.Close()
 
-	err = s.UpdateAvatar(&req, p)
-	if err != nil {
-		e.Logger.Error(err)
-		return
+		// Upload the file to MinIO
+		_, err = minioClient.PutObject(context.Background(), bucketName, fileName, src, file.Size, minio.PutObjectOptions{ContentType: file.Header.Get("Content-Type")})
+		if err != nil {
+			e.Logger.Errorf("upload to minio error, %s", err.Error())
+			e.Error(500, err, "")
+			return
+		}
+
+		// Construct the MinIO file URL
+		minioURL := urlPrefix + bucketName + "/" + fileName
+		req.UserId = p.UserId
+		req.Avatar = minioURL
+
+		err = s.UpdateAvatar(&req, p)
+		if err != nil {
+			e.Logger.Error(err)
+			return
+		}
 	}
-	e.OK(filPath, "修改成功")
+
+	e.OK(req.Avatar, "修改成功")
 }
 
 // UpdateStatus 修改用户状态
@@ -434,9 +521,9 @@ func (e SysUser) GetInfo(c *gin.Context) {
 		mp["permissions"] = permissions
 		mp["buttons"] = buttons
 	} else {
-		list, _ := r.GetById(user.GetRoleId(c))
-		mp["permissions"] = list
-		mp["buttons"] = list
+		//list, _ := r.GetById(user.GetRoleId(c))
+		mp["permissions"] = nil
+		mp["buttons"] = nil
 	}
 	sysUser := models.SysUser{}
 	req.Id = user.GetUserId(c)
@@ -453,74 +540,25 @@ func (e SysUser) GetInfo(c *gin.Context) {
 	mp["userName"] = sysUser.NickName
 	mp["userId"] = sysUser.UserId
 	mp["deptId"] = sysUser.DeptId
+	//mp["deptName"] = sysUser.Dept.DeptName
 	mp["name"] = sysUser.NickName
 	mp["code"] = 200
 	e.OK(mp, "")
 }
-
-//func Register(ctx *gin.Context) {
-//	DB := utils.GetDB()
-//	redisdb := utils.InitRedis()
-//	//获取参数
-//	name := ctx.PostForm("username") //注意字符串要用双引号
-//	password := ctx.PostForm("password")
-//	email := ctx.PostForm("email")
-//	Emailcode := ctx.PostForm("EmailCode")
-//
-//	hasedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-//	if err != nil {
-//		//ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 500, "msg": "加密错误"})
-//		common.ResErr(ctx, "加密错误")
-//		return
-//	}
-//	newUser := models.SysUser{
-//		Username: name,
-//		Password: string(hasedPassword), //将加密后的密码保存起来
-//		Email:    email,
-//		DeptId:   1,
-//		Status:   strconv.Itoa(2),
-//		RoleId:   3,
-//	}
-//	result, err := redisdb.Get(email).Result()
-//	if err != nil {
-//		// 处理错误情况
-//		common.ResErr(ctx, "请先获取验证码！")
-//		return
-//	}
-//	if result == Emailcode {
-//		DB.Create(&newUser)
-//		common.ResOK(ctx, "注册成功！")
-//		return
-//	}
-//	if result != Emailcode {
-//		common.ResErr(ctx, "验证码错误！")
-//		return
-//	}
-//	common.ResErr(ctx, "注册失败！")
-//}
 
 /**
  * 注册接口
  * @param Register
  * @return
  */
-func (e SysUser) Register(c *gin.Context) {
-	//获取参数
-	name := c.PostForm("username") //注意字符串要用双引号
-	password := c.PostForm("password")
-	email := c.PostForm("email")
-	Emailcode := c.PostForm("EmailCode")
 
+func (e SysUser) Register(c *gin.Context) {
 	s := service.SysUser{}
 	req := dto.Register{}
 
-	req.Username = name
-	req.Password = password
-	req.Email = email
-	req.Emailcode = Emailcode
-	req.SeRoleId(3)
-	req.SetDeptId(1)
-	req.SetStatus(2)
+	req.RoleId = 3
+	req.DeptId = 1
+	req.Status = strconv.Itoa(2)
 
 	err := e.MakeContext(c).
 		MakeOrm().
@@ -542,105 +580,58 @@ func (e SysUser) Register(c *gin.Context) {
 }
 
 /**
- * 登录接口
- * @param Login
+ * @e无需权限的更新（//TODO 换为需要权限的更新接口）
+ * @Param
  * @return
- */
-func Login(c *gin.Context) {
-	DB := utils.GetDB()
-	//获取参数
-	username := c.PostForm("name")
-	password := c.PostForm("password")
+ * @Date 2024/8/20 下午2:37
+ **/
 
-	if len(password) < 6 {
-		//ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于6位"})
-		common.ResErr(c, "密码不能少于6位")
+func (e SysUser) UpdateUser(c *gin.Context) {
+	s := service.SysUser{}
+	req := models.SysUser{}
+	err := e.MakeContext(c).
+		MakeOrm().
+		Bind(&req).
+		MakeService(&s.Service).
+		Errors
+	if err != nil {
+		e.Logger.Error(err)
+		e.Error(500, err, err.Error())
 		return
 	}
 
-	var user models.SysUser
-	DB.Where("username = ?", username).First(&user)
-	if user.UserId == 0 {
-		common.ResErr(c, "用户不存在")
+	err = s.UpdateUser(&req)
+	if err != nil {
+		e.Logger.Error(err)
 		return
 	}
-	//判断密码是否正确
-	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
-		//ctx.JSON(http.StatusBadRequest, gin.H{"code": 400, "msg": "密码错误"})
-		common.ResErr(c, "密码错误")
-		return
-	}
-	common.SetCookie(username, c)
-	common.ResOK(c, "登录成功")
+	e.OK(req.GetId(), "更新成功")
 }
 
-func Start(c *gin.Context) {
-	uname, _ := c.Get("user_name")
-	db := utils.GetDB()
-	currentDate := time.Now().Format("2006-01-02")
-	currentTime := time.Now().Format("15:04:05")
-	// 查询当前用户当天是否有打卡记录，有则更新，无则新增
-	var duration models.Durations
-	db.Where("name = ? AND date = ?", uname, currentDate).First(&duration)
-	// 开始时间不为空，说明正在计时中
-	if duration.StartTime != "" {
-		common.ResOK(c, "已完成开始打卡")
-		return
-	}
-	// 开始时间为空则判断今日是否打过卡，打过卡说明已经创建过记录，把记录更新即可
-	if duration.ID != 0 {
-		db.Model(&duration).Update("start_time", currentTime)
-		common.ResOK(c, "开始打卡成功")
-		return
-	}
-	// 未打过卡就新增本日记录
-	newDur := models.Durations{
-		Name:      uname.(string),
-		Date:      currentDate,
-		StartTime: currentTime,
-		Dur:       0,
-	}
-	db.Create(&newDur)
-	common.ResOK(c, "开始打卡成功")
-}
+/**
+ * @根据用户id查询数据
+ * @Param
+ * @return
+ * @Date 2024/8/21 上午12:37
+ **/
 
-func End(c *gin.Context) {
-	uname, _ := c.Get("user_name")
-	db := utils.GetDB()
-	currentDate := time.Now().Format("2006-01-02")
-	currentTime := time.Now().Format("15:04:05")
-	// 查询当前用户当天是否有打卡记录
-	var duration models.Durations
-	db.Where("name = ? AND date = ?", uname, currentDate).First(&duration)
-	// 提示未进行开始打卡
-	if duration.StartTime == "" {
-		common.ResErr(c, "未进行开始打卡")
+func (e SysUser) GetByUserId(c *gin.Context) {
+	userid := c.Query("userid")
+	s := service.SysUser{}
+	err := e.MakeContext(c).
+		MakeOrm().
+		MakeService(&s.Service).
+		Errors
+	if err != nil {
+		e.Logger.Error(err)
+		e.Error(500, err, err.Error())
 		return
 	}
-	// 结算本次打卡时间
-	t1, err1 := time.Parse("15:04:05", duration.StartTime)
-	t2, err2 := time.Parse("15:04:05", currentTime)
-	if err1 != nil || err2 != nil {
-		common.ResErr(c, "时间解析出错")
+
+	data, err := s.GetByUserId(userid)
+	if err != nil {
+		e.Logger.Error(err)
 		return
 	}
-	durObj := t2.Sub(t1)
-	dur := int(durObj.Seconds())
-	todayDur := duration.Dur + dur
-	// 更新本日记录
-	db.Model(&duration).Updates(map[string]interface{}{
-		"StartTime": "",
-		"Dur":       todayDur,
-	})
-	// 返回具体时分秒
-	h := dur / 3600
-	m := (dur % 3600) / 60
-	s := dur % 60
-	c.JSON(http.StatusOK, gin.H{
-		"code":    200,
-		"msg":     "结束打卡成功",
-		"hours":   h,
-		"minutes": m,
-		"seconds": s,
-	})
+	e.OK(data, "查看用户信息成功")
 }
